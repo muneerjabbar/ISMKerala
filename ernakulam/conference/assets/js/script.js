ec66e1107ea396054a13e1c532042a9efc3b9373
muneerpj <muneer.jabbar@gmail.com>
2025-11-02 06:45:23 +0530

    live link updated

--- ernakulam/conference/assets/js/script.js
+++ ernakulam/conference/assets/js/script.js
@@ -5,7 +5,7 @@
         frameborder="0"
         allow="autoplay; encrypted-media"
         allowfullscreen
-        src="https://www.youtube.com/embed/5AihsUz6Jds?autoplay=1">
+        src="https://www.youtube.com/live/WotzyrlhHL0?autoplay=1">
       </iframe>
     `;
   }
