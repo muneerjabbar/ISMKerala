ea96059a0acacb98f24baf66d41c257c84708281
muneerpj <muneer.jabbar@gmail.com>
2026-02-09 10:08:48 +0530

    updated ernakulam qhls new convenors

--- ernakulam/qhls/assets/js/script.js
+++ ernakulam/qhls/assets/js/script.js
@@ -1,158 +1,125 @@
 // 🕌 QHLS Centers Data as JSON
 const qhlsData = [
   {
-    zone: "Kakkanad",
-    convenor: { name: "Riyas", phone: "8891110025" },
-    units: [
-      { name: "Kakkanad", convenor: "Riyas", phone: "8891110025" },
-      { name: "Athani", convenor: "Muneer", phone: "9400095648" },
-      { name: "Kalamassery", convenor: "Dr Ajees", phone: "9061859697" },
-      { name: "Padamukal", convenor: "Muhammad Babu", phone: "9061859697" },
-      { name: "Edappalli", convenor: "Mayin", phone: "9061859697" },
-      { name: "Cheranalloor", convenor: "Shebin", phone: "9061859697" },
-      { name: "Edappalli", convenor: "Mayin", phone: "9061859697" },
-      { name: "Edappalli", convenor: "Mayin", phone: "9061859697" },
-    ]
-  },  
-  {
-    zone: "Kochi",
-    convenor: { name: "Haris Muhammed", phone: "8891110025" },
-    units: [
-      { name: "Kochi", convenor: "Junaid K", phone: "8891110025" },
-      { name: "Mattanchery", convenor: "Shafeeq A", phone: "8891110025" },
-      { name: "Cheralayikkadavu", convenor: "Noufal K", phone: "8891110025" },
-      { name: "C P Thodu", convenor: "Riyas K", phone: "8891110025" },
-      { name: "Kunnumpuram", convenor: "Muhsin T", phone: "8891110025" },
-      { name: "Kalvathi", convenor: "Rafeeq A", phone: "8891110025" },
-      { name: "Thuruthi", convenor: "Ashiq H", phone: "8891110025" },
-      { name: "Mini Market", convenor: "Shameer C", phone: "8891110025" },
-      { name: "Pattalam", convenor: "Nihal V", phone: "8891110025" },
-      { name: "Thoppumpady", convenor: "Jaseem P", phone: "8891110025" }
+    "zone": "ALUVA",
+    "convenor": { "name": "ASHKAR K A", "phone": "9037553423" },
+    "units": [
+      { "name": "Aluva", "convenor": "Ameen Shageer", "phone": "7736124227" },
+      { "name": "ELOOKARA", "convenor": "Shanavas ka", "phone": "9995380794" },
+      { "name": "Kunnatheri", "convenor": "MUFEEDUL AVAM P.M", "phone": "9074330471" },
+      { "name": "Kuzhivelippadi", "convenor": "Anees v m", "phone": "9037222421" },
+      { "name": "NALAMMILE", "convenor": "Muhammed Shahid k", "phone": "8943118023" },
+      { "name": "Nochima", "convenor": "Niyas K A", "phone": "9947635343" },
+      { "name": "Sreemoolanagaram", "convenor": "Muhammed Adhil T A", "phone": "8547809278" },
+      { "name": "THAIKKATTUKARA", "convenor": "Mohammed Al-haamid", "phone": "9995309493" },
+      { "name": "THOUHEED NAGAR", "convenor": "Ahlas ibnu subair", "phone": "7510352615" },
+      { "name": "Uliyannur", "convenor": "Muhammed Asif m.y", "phone": "8590277804" }
     ]
   },
   {
-    zone: "Palluruthy",
-    convenor: { name: "Riyas A", phone: "8891110025" },
-    units: [
-      { name: "East Shakha", convenor: "Jaleel P", phone: "8891110025" },
-      { name: "Perumbadappu", convenor: "Shabin T", phone: "8891110025" },
-      { name: "Nambyapuram", convenor: "Naseer K", phone: "8891110025" },
-      { name: "Kacherippadi", convenor: "Hassan M", phone: "8891110025" }
+    "zone": "ERNAKULAM",
+    "convenor": { "name": "ANSARI ISLAHI", "phone": "8848225907" },
+    "units": [
+      { "name": "Kaloor", "convenor": "FIAZ MUHAMMED", "phone": "9072464543" }
     ]
   },
   {
-    zone: "Ernakulam",
-    convenor: { name: "Shameem P", phone: "8891110025" },
-    units: [
-      { name: "Kaloor", convenor: "Ameen P", phone: "8891110025" },
-      { name: "Mulavukad", convenor: "Basil K", phone: "8891110025" },
-      { name: "Kombara", convenor: "Salim M", phone: "8891110025" },
-      { name: "SRM Road", convenor: "Fayiz A", phone: "8891110025" },
-      { name: "Ellamakkara", convenor: "Rashid N", phone: "8891110025" },
-      { name: "Pullepadi", convenor: "Anas C", phone: "8891110025" },
-      { name: "Kathrikadavu", convenor: "Rahman V", phone: "8891110025" }
+    "zone": "KAKKANAD",
+    "convenor": { "name": "Riyas p m", "phone": "8891110025" },
+    "units": [
+      { "name": "Athani", "convenor": "Muhammed Ibnu M.N", "phone": "7356691437" },
+      { "name": "Cheranallur", "convenor": "Syed Mohammed bin yahya", "phone": "9947888003" },
+      { "name": "Edappalli", "convenor": "Mahin abdul salam", "phone": "9947914955" },
+      { "name": "Infopark", "convenor": "Yasar A P", "phone": "9884088118" },
+      { "name": "Kakkanad", "convenor": "Riyas p m", "phone": "8891110025" },
+      { "name": "Kalamassery", "convenor": "Dr. Ajees A P", "phone": "9061859697" },
+      { "name": "Kangarappadi", "convenor": "Salahudheen V M", "phone": "8089371379" },
+      { "name": "Mattakkad", "convenor": "Akbar p a", "phone": "9895522461" },
+      { "name": "Padamugal", "convenor": "MOHAMMED BABU M. H", "phone": "8089100778" }
     ]
   },
   {
-    zone: "Vyttila",
-    convenor: { name: "Rashid Ahamed", phone: "8891110025" },
-    units: [
-      { name: "Chakkaraparambu", convenor: "Muhsin P", phone: "8891110025" },
-      { name: "Nettur", convenor: "Hameed K", phone: "8891110025" },
-      { name: "Vyttila", convenor: "Jaleel R", phone: "8891110025" },
-      { name: "Kanjiramattom", convenor: "Rafeeq T", phone: "8891110025" },
-      { name: "Kulashekaramangalam", convenor: "Suhail M", phone: "8891110025" },
-      { name: "Thalayolaparambu", convenor: "Shameer V", phone: "8891110025" },
-      { name: "Kumbalam", convenor: "Afsal C", phone: "8891110025" },
-      { name: "Vettikkattumool", convenor: "Shamsudheen A", phone: "8891110025" }
+    "zone": "KOCHI",
+    "convenor": { "name": "Haris v. M", "phone": "8089657667" },
+    "units": [
+      { "name": "CALVATHY", "convenor": "Looth MS", "phone": "8593928200" },
+      { "name": "Cheralayi Kadavu", "convenor": "Mujahid minhaj", "phone": "8129019244" },
+      { "name": "CP THODU", "convenor": "Shaheer thailanil Ebrahim", "phone": "9895877967" },
+      { "name": "Kochi", "convenor": "ABDUL FAIZ M.H.", "phone": "8714938880" },
+      { "name": "Kunnumpuram", "convenor": "Aneesh M A", "phone": "9895116564" },
+      { "name": "Mattanchery", "convenor": "Abdul shaheer", "phone": "8086018813" },
+      { "name": "PATTALLAM", "convenor": "Shaik mujeeb rahman", "phone": "8089557919" },
+      { "name": "THURUTHY", "convenor": "Mohamed afreed", "phone": "9447140582" }
     ]
   },
   {
-    zone: "Aluva",
-    convenor: { name: "Basheer K", phone: "8891110025" },
-    units: [
-      { name: "Elookkara", convenor: "Nihal M", phone: "8891110025" },
-      { name: "Thottakkattukara", convenor: "Suhail V", phone: "8891110025" },
-      { name: "Sreemoolanagaram", convenor: "Afsal C", phone: "8891110025" },
-      { name: "Sreerbhuthapuram", convenor: "Shamsu P", phone: "8891110025" },
-      { name: "Kuttamassery", convenor: "Rashid T", phone: "8891110025" },
-      { name: "Edayappuram", convenor: "Shameem A", phone: "8891110025" },
-      { name: "Naalam Mile", convenor: "Haris M", phone: "8891110025" },
-      { name: "Kuzhivelippadi", convenor: "Jaseem N", phone: "8891110025" },
-      { name: "Nochima", convenor: "Basil P", phone: "8891110025" },
-      { name: "Kunnatheri", convenor: "Ashraf K", phone: "8891110025" },
-      { name: "Thaikkattukara", convenor: "Rafeeq C", phone: "8891110025" },
-      { name: "Muttam", convenor: "Sadiq A", phone: "8891110025" },
-      { name: "Veliyannur", convenor: "Mansoor T", phone: "8891110025" },
-      { name: "Kodungallur", convenor: "Suhail P", phone: "8891110025" },
-      { name: "Millupadi", convenor: "Anwar K", phone: "8891110025" }
+    "zone": "KOTHAMANGALAM",
+    "convenor": { "name": "Abdul Kareem", "phone": "7594846607" },
+    "units": [
+      { "name": "ADIVAD", "convenor": "Ansar Ali", "phone": "9946651310" },
+      { "name": "Kothamangalam", "convenor": "Sahal Ali", "phone": "9745556962" },
+      { "name": "Manikkinar", "convenor": "Firoz Abdullah", "phone": "9947605439" },
+      { "name": "NELLIKUZHY", "convenor": "Thanzeer K M", "phone": "9092121678" },
+      { "name": "PAIMATTOM", "convenor": "Abdul Kareem", "phone": "7594846607" }
     ]
   },
   {
-    zone: "Paravur",
-    convenor: { name: "Anas M", phone: "8891110025" },
-    units: [
-      { name: "Alamthuruth", convenor: "Rashid K", phone: "8891110025" },
-      { name: "Manjali", convenor: "Nabeel A", phone: "8891110025" },
-      { name: "Purayar", convenor: "Hisham C", phone: "8891110025" },
-      { name: "Veliyanad", convenor: "Shafeek T", phone: "8891110025" },
-      { name: "Panayikkulam", convenor: "Shameer N", phone: "8891110025" },
-      { name: "Kunnukara", convenor: "Riyas M", phone: "8891110025" }
+    "zone": "MUVATTUPUZHA",
+    "convenor": { "name": "Noufal VA", "phone": "9400916174" },
+    "units": [
+      { "name": "KIZHAKKEKKARA", "convenor": "HARIS S", "phone": "9446395132" },
+      { "name": "MULAVOOR", "convenor": "Naisal k.k", "phone": "9961845161" },
+      { "name": "Muvattupuzha Town", "convenor": "Mujeeb Moosa", "phone": "9605507249" },
+      { "name": "Payipra", "convenor": "Afsal fouzi", "phone": "9847896353" },
+      { "name": "Perumattom", "convenor": "faris kabeer", "phone": "9446726587" }
     ]
   },
   {
-    zone: "Perumbavoor",
-    convenor: { name: "Jaleel K", phone: "8891110025" },
-    units: [
-      { name: "Minikkavala", convenor: "Muneer P", phone: "8891110025" },
-      { name: "Chelakkulam", convenor: "Shameem V", phone: "8891110025" },
-      { name: "Pattimattam", convenor: "Ameen T", phone: "8891110025" },
-      { name: "Arakkappadi", convenor: "Suhail K", phone: "8891110025" },
-      { name: "Pothanam Parambu", convenor: "Rashid A", phone: "8891110025" },
-      { name: "Parakkode", convenor: "Hassan C", phone: "8891110025" },
-      { name: "Marampilly", convenor: "Jaseem V", phone: "8891110025" },
-      { name: "Vengola", convenor: "Basheer T", phone: "8891110025" },
-      { name: "Perumbavoor", convenor: "Mansoor M", phone: "8891110025" },
-      { name: "Pallikkara", convenor: "Nihal C", phone: "8891110025" },
-      { name: "Pallikkavala", convenor: "Anwar P", phone: "8891110025" }
+    "zone": "PALLURUTHI",
+    "convenor": { "name": "Ummermukthar km", "phone": "7736563613" },
+    "units": [
+      { "name": "East Shakha", "convenor": "Siraj.p.s.", "phone": "9061979978" },
+      { "name": "Edakochi", "convenor": "Shameer AJ", "phone": "7994043271" },
+      { "name": "KACHERIPADY", "convenor": "ALTHAF", "phone": "8137963317" },
+      { "name": "Nambyapuram", "convenor": "MUNEER KM", "phone": "9946025999" }
     ]
   },
   {
-    zone: "Kothamangalam",
-    convenor: { name: "Hameed M", phone: "8891110025" },
-    units: [
-      { name: "Paimattam", convenor: "Suhail C", phone: "8891110025" },
-      { name: "Nellikuzhi", convenor: "Naseer K", phone: "8891110025" },
-      { name: "Irumalappadi", convenor: "Afsal P", phone: "8891110025" },
-      { name: "Kothamangalam", convenor: "Fayiz T", phone: "8891110025" },
-      { name: "Manikkinar", convenor: "Shameer A", phone: "8891110025" },
-      { name: "Pallarimangalam", convenor: "Rahman P", phone: "8891110025" },
-      { name: "Adivadu", convenor: "Basheer N", phone: "8891110025" }
+    "zone": "PARAVOOR",
+    "convenor": { "name": "Muhammed jabir", "phone": "9961613714" },
+    "units": []
+  },
+  {
+    "zone": "PERUMBAVOOR",
+    "convenor": { "name": "ANSAL M V", "phone": "9946104727" },
+    "units": [
+      { "name": "MINIKKAVALA", "convenor": "ASHKAR A S", "phone": "7025162366" },
+      { "name": "Pallikkara Paracode", "convenor": "Arafath M.V", "phone": "9446470755" },
+      { "name": "Pallikkavala", "convenor": "MUHAMMED ABDUL KAYYUM", "phone": "9188213332" }
     ]
   },
   {
-    zone: "Muvattupuzha",
-    convenor: { name: "Iqbal", phone: "9526155839" },
-    units: [
-      { name: "Punnamattam", convenor: "Mansoor A", phone: "8891110025" },
-      { name: "Paipra", convenor: "Iqbal", phone: "9526155839" },
-      { name: "Perumattam", convenor: "Suhail K", phone: "8891110025" },
-      { name: "Kizhakkekkara", convenor: "Anas T", phone: "8891110025" },
-      { name: "Mulavoor", convenor: "Haris P", phone: "8891110025" },
-      { name: "Muvattupuzha", convenor: "Noufal", phone: "9400916174" }
-    ]
+    "zone": "VYPPIN",
+    "convenor": { "name": "Nizamudheen", "phone": "7736472276" },
+    "units": []
   },
   {
-    zone: "Vypin",
-    convenor: { name: "Shameer A", phone: "8891110025" },
-    units: [
-      { name: "Edavanakkad", convenor: "Riyas M", phone: "8891110025" },
-      { name: "Malippuram", convenor: "Naseef K", phone: "8891110025" },
-      { name: "PuthuVypin", convenor: "Afsal P", phone: "8891110025" },
-      { name: "Illathuppadi", convenor: "Shameem T", phone: "8891110025" }
+    "zone": "VYTTILA",
+    "convenor": { "name": "AJMAL ALTHAF MH", "phone": "8714251522" },
+    "units": [
+      { "name": "Chakkaraparambu", "convenor": "Fahad P J", "phone": "9633747200" },
+      { "name": "Kanjiramattom", "convenor": "Aamirul Ameen K M", "phone": "8848962147" },
+      { "name": "KULASEKARAMANGALAM", "convenor": "Anshad.k.a", "phone": "9746117113" },
+      { "name": "KULASEKARAMANGALAM", "convenor": "NISHAD. K.U", "phone": "7907259233" },
+      { "name": "Kumbalam", "convenor": "Sakkeer cm", "phone": "9895308677" },
+      { "name": "NETOOR", "convenor": "Badusha Salim", "phone": "9746122002" },
+      { "name": "Thalayolapparambu", "convenor": "Sinan Muhammed", "phone": "8606643365" },
+      { "name": "VETTIKKATTUMUKKU", "convenor": "Irfan T Y", "phone": "9633192983" },
+      { "name": "Vyttila", "convenor": "Fazal Mujeeb", "phone": "8714071610" }
     ]
   }
-];
+]
+;
 
 
 // 🧱 Render Accordion Dynamically
